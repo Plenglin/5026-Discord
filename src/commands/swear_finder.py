3c630ecd1450772f23a3f93d1af0b70324ceb92d
Astrid Augusta Yu <astridyu3.14@gmail.com>
2017-04-28 12:25:35 -0700

    Add a way for users to know how much they have sweared

--- src/commands/swear_finder.py
+++ src/commands/swear_finder.py
@@ -1,6 +1,7 @@
 import os
 import random
 import logging
+import re
 
 import psycopg2 as pg
 
@@ -30,7 +31,7 @@ class SwearJar(bot_framework.Listener):
     async def on_message(self, message):
 
         if any(w in message.content for w in SWEAR_WORDS):
-            log.debug('detected a swear')
+            log.info('Detected a swear from %s', message.author.name)
             await self.client.send_message(message.channel, random.choice(MESSAGES).format(mention=message.author.mention))
             cursor = self.connection.cursor()
             occurences = sum(message.content.count(w) for w in SWEAR_WORDS)
@@ -42,15 +43,33 @@ class SwearJar(bot_framework.Listener):
             cursor.execute("UPDATE swears SET times_sweared = times_sweared + {} WHERE discord_id = '{}'".format(occurences, message.author.id))
             self.connection.commit()
 
-        elif self.client.user in message.mentions and 'how many times' in message.content and 'sweared' in message.content:
-            try:
-                user = next(u for u in message.mentions if u != self.client.user)
-            except StopIteration:
-                user = message.author
-            cursor = self.connection.cursor()
-            cursor.execute("SELECT times_sweared FROM swears WHERE discord_id = '{}'".format(user.id))
-            data = cursor.fetchone()
-            if data in (None, 0):
-                await self.client.send_message(message.channel, '{} has not sworn at all.'.format(user.mention))
-            else:
-                await self.client.send_message(message.channel, '{} has sworn {} times.'.format(user.mention, data[0]))
+        elif self.client.user in message.mentions and re.search(r'swore|swear', message.content):
+            if 'how many times' in message.content:
+                log.info('%s wants to know # of swears', message.author.name)
+                try:
+                    user = next(u for u in message.mentions if u != self.client.user)
+                except StopIteration:
+                    user = message.author
+                cursor = self.connection.cursor()
+                cursor.execute("SELECT times_sweared FROM swears WHERE discord_id = '{}'".format(user.id))
+                data = cursor.fetchone()
+                if data in (None, 0):
+                    await self.client.send_message(message.channel, '{} has not sworn at all.'.format(user.mention))
+                else:
+                    await self.client.send_message(message.channel, '{} has sworn {} times.'.format(user.mention, data[0]))
+            elif all(w in message.content for w in 'who most'.split()):
+                log.info('%s wants a leaderboard', message.author.name)
+                server = message.channel.server
+                cursor = self.connection.cursor()
+                cursor.execute('SELECT discord_id, times_sweared FROM swears ORDER BY times_sweared DESC;')
+                size = min(5, len(server.members))
+                server_ids = [u.id for u in server.members]
+                data_in_server = ((server.get_member(i), t) for i, t in cursor if i in server_ids)
+                result = [next(data_in_server, None) for _ in range(0, size)]
+                output = ''
+                for rank, p in enumerate(result):
+                    if p is None:
+                        break
+                    user, times = p
+                    output += '#{rank}: {name}, with {times} swears\n'.format(rank=rank+1, name=user.display_name, times=times)
+                await self.client.send_message(message.channel, output)
